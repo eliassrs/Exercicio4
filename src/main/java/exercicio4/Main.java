9bff60c34c7ade239e8824b5806d6a82b24c1986
Elias Moreira <moreira-s@hotmail.com>
2021-04-11 14:14:32 -0300

    primeiro commit

---
+++ src/main/java/exercicio4/Main.java
@@ -0,0 +1,35 @@
+package exercicio4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main exercicio = new Main();
+		exercicio.iniciar();
+		
+	}
+
+	private void iniciar() {
+		int i=20;
+		while(i>0) {
+		System.out.println(i);
+		i--;
+		System.out.println();
+		
+		int k=25;
+		do {
+			System.out.println(k);
+			k++;
+			
+		}
+		while (k<25);
+		
+		for(int j=7;j<8;j++) 
+		     System.out.println(j);
+		
+		}
+		
+	}
+
+}
+	
+
